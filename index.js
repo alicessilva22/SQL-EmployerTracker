1720ff15627e1d296979704da2009f62c09bb704
Alice Silva <alicessilva@aol.com>
2022-06-24 18:31:21 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'classlist_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
+
+db.query('SELECT * FROM students', function (err, results) {
+  console.log(results);
+});
