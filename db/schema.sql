66d05ca14385386ea66b4c06cd49eadf7045f317
Alice Silva <alicessilva@aol.com>
2022-06-25 09:45:22 -0400

    schema created

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,23 @@
-department
+CREATE DATABASE department_db;
 
-id: INT PRIMARY KEY
+USE department_db;
 
-name: VARCHAR(30) to hold department name
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30) NOT NULL,
+);
 
-role
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+role_title VARCHAR(30) NOT NULL,
+role_salary INT NOT NULL,
+department_id INT NOT NULL,
+);
 
-id: INT PRIMARY KEY
-
-title: VARCHAR(30) to hold role title
-
-salary: DECIMAL to hold role salary
-
-department_id: INT to hold reference to department role belongs to
-
-employee
-
-id: INT PRIMARY KEY
-
-first_name: VARCHAR(30) to hold employee first name
-
-last_name: VARCHAR(30) to hold employee last name
-
-role_id: INT to hold reference to employee role
-
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+);
