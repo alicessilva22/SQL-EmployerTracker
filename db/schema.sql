be793326d27574af9a3c1b3dbb0695508c108572
Alice Silva <alicessilva@aol.com>
2022-06-27 19:23:38 -0400

    added prompts

--- db/schema.sql
+++ db/schema.sql
@@ -16,9 +16,9 @@ department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT
 );
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('console.table');
 
 const db = mysql.createConnection(
   {
@@ -8,100 +9,145 @@ const db = mysql.createConnection(
     password: '',
     database: 'department_db'
   },
-  console.log(`Connected to the department_db database.`)
 );
 
 
 const viewDepartments = () => {
-  db.query('SELECT departments * FROM department', function (err, results) {
+  db.query('SELECT * FROM department', function (err, results) {
     if (err) return console.error(err);
-    return console.table(results);
+    console.table(results);
+    return init();
   });
 };
 const viewRoles = () => {
   db.query('SELECT * FROM role', function (err, results) {
     if (err) return console.error(err);
-    return console.table(results);
+    console.table(results);
+    return init();
   });
 };
 const viewEmployees = () => {
   db.query('SELECT * FROM employee', function (err, results) {
     if (err) return console.error(err);
-    return console.table(results);
+    console.table(results);
+    return init();
   });
 };
 const addDepartment = () => {
-  db.query('SELECT * FROM department', function (err, results) {
-    if (err) return console.error(err);
-    return console.table(results);
-  });
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'deptName',
+      message: 'What is the name of the new department?'
+    }
+  ]).then(answer => {
+    db.query('INSERT INTO department SET ?;', 
+    {
+      name: answer.deptName
+    });
+  })
 };
 const addRole = () => {
-  db.query('INSERT INTO department', function (err, results) {
+  db.query('INSERT INTO role SET ?', function (err, results) {
     if (err) return console.error(err);
-    return console.table(results);
+    console.table(results);
+    return init();
   });
 };
 const addEmployee = () => {
-  db.query('INSERT INTO employee', function (err, results) {
+  db.query('INSERT INTO employee SET?', function (err, results) {
     if (err) return console.error(err);
-    return console.table(results);
+    console.table(results);
+    return init();
   });
 };
 const updateRole = () => {
-  db.query('SELECT * FROM role', function (err, results) {
-    if (err) return console.error(err);
-    return console.table(results);
+  db.query('SELECT * FROM employee', (err, res) => {
+    // console.log(res)
+
+    // let employeeNames = []
+    // for(let i =0; i < res.length; i++) {
+    //   employeeNames.push(res[i].first_name)
+    // }
+    let allEmployeeData = res.map(({id, first_name}) => ({
+      name: first_name,
+      value: id
+    }))
+    console.log(allEmployeeData);
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'emp',
+        message: 'What employee are you updating?',
+        choices: allEmployeeData
+      },
+      {
+        type: 'input',
+        name: 'newRole',
+        message: 'What is this employees new role id?'
+      }
+    ]).then(answers => {
+      db.query('UPDATE employee SET ? WHERE ?', 
+      {
+        id: answers.emp,
+        role_id: answers.newRole
+      })
+    })
+  })
+};
+
+const init = () => {
+  inquirer.prompt([
+    {
+      type: 'rawlist',
+      name: 'questions',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+      ]
+    }
+  ]).then((answers) => {
+    switch (answers.questions) {
+      case 'View all departments': {
+        viewDepartments();
+        break;
+      }
+      case 'View all roles': {
+        viewRoles();
+        break;
+      }
+      case 'View all employees': {
+        viewEmployees();
+        break;
+      }
+      case 'Add a department': {
+        addDepartment();
+        break;
+      }
+      case 'Add a role': {
+        addRole();
+        break;
+      }
+      case 'Add an employee': {
+        addEmployee();
+        break;
+      }
+      case 'Update an employee role': {
+        updateRole();
+        break;
+      }
+      default: {
+        process.exit();
+      }
+    }
   });
 };
 
-inquirer.prompt([
-  {
-    type: 'rawlist',
-    name: 'questions',
-    message: 'What would you like to do?',
-    choices: [
-      'View all departments',
-      'View all roles',
-      'View all employees',
-      'Add a department',
-      'Add a role',
-      'Add an employee',
-      'Update an employee role',
-    ]
-    }
-]).then((answers) => {
-  switch(answers.query) {
-    case 'View all departments': {
-      viewDepartments();
-      break;
-    }
-    case 'View all roles': {
-      viewRoles();
-      break;
-    }
-    case 'View all employees': {
-      viewEmployees();
-      break;
-    }
-    case 'Add a department': {
-      addDepartment();
-      break;
-    }
-    case 'Add a role': {
-      addRole();
-      break;
-    }
-    case 'Add an employee': {
-      addEmployee();
-      break;
-    }
-    case 'Update an employee role': {
-      updateRole();
-      break;
-    }
-    case 'Finish': {
-
-    }
-  }
-});
+init();
