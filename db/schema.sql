82ea95a30d93d05d47aeea1126c81752fb7f3af0
Alice Silva <alicessilva@aol.com>
2022-06-27 08:37:17 -0400

    debugged schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE department_db;
 
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR(30) NOT NULL,
+department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 role_title VARCHAR(30) NOT NULL,
 role_salary INT NOT NULL,
-department_id INT NOT NULL,
+department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
@@ -20,5 +20,5 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
+    manager_id INT
 );
